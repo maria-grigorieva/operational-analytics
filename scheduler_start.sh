b097a2356addc29b74da71d4da712c97071d0757
maria <magsend@gmail.com>
2022-10-26 14:44:33 +0200

    path to celery has been changed

--- scheduler_start.sh
+++ scheduler_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # start Celery workers
-nohup /opt/miniconda3/envs/analytix/bin/celery -A workers worker -l info -f logs/celery.log > /dev/null 2>&1&
+nohup /opt/celery_venv/bin/celery -A workers worker -l info -f logs/celery.log > /dev/null 2>&1&
 echo "Celery Worker started"
 
 # start Celerybeat
-nohup /opt/miniconda3/envs/analytix/bin/celery -A workers beat -l info -f logs/celerybeat.log > /dev/null 2>&1&
+nohup /opt/celery_venv/bin/celery -A workers beat -l info -f logs/celerybeat.log > /dev/null 2>&1&
 echo "Celerybeat started"
 
 # start Flower
-nohup /opt/miniconda3/envs/analytix/bin/celery -A workers flower --port=5555 -l info -f logs/flower.log > /dev/null 2>&1&
+nohup /opt/celery_venv/bin/celery -A workers flower --port=5555 -l info > /dev/null 2>&1&
 echo "Flower started"
