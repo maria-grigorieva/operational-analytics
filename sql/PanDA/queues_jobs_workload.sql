60cb09a8917c53dc5aa1757f75c5432a17d94061
Maria Grigorieva <magsend@gmail.com>
2022-12-21 17:43:23 +0300

    [refactor] Refactored Queues Jobs Workload SQL Query

--- sql/PanDA/queues_jobs_workload.sql
+++ sql/PanDA/queues_jobs_workload.sql
@@ -1,156 +1,257 @@
-with jobs_statuses as (SELECT s.pandaid,
-           s.computingsite                                                as queue,
-           s.jobstatus                                                    as status,
-           s.modificationtime
-    FROM ATLAS_PANDA.JOBS_STATUSLOG s
-    WHERE s.modificationtime >=
-          (trunc(to_date(:from_date, 'YYYY-MM-DD HH24:MI:SS'), 'HH24') - 1/24)
-      AND s.modificationtime < trunc(to_date(:from_date, 'YYYY-MM-DD HH24:MI:SS'), 'HH24')
-    AND s.prodsourcelabel = 'user'
-    ),
-    prev as (
-        SELECT pandaid,
-               queue,
-               status,
-               modificationtime
-               FROM (SELECT pandaid,
-                            computingsite                                                            as queue,
-                            jobstatus                                                                as status,
-                            (trunc(to_date(:from_date, 'YYYY-MM-DD HH24:MI:SS'), 'HH24') - 1 / 24) as modificationtime,
-                            ROW_NUMBER() OVER (PARTITION BY pandaid ORDER BY modificationtime desc) AS rn
-                       FROM ATLAS_PANDA.JOBS_STATUSLOG
-                       WHERE pandaid in (SELECT distinct pandaid from jobs_statuses)
-                          AND modificationtime < (trunc(to_date(:from_date, 'YYYY-MM-DD HH24:MI:SS'), 'HH24') - 1 / 24))
-        WHERE rn in 1),
-    merge as (
-            SELECT pandaid,
-                     queue,
-                     status,
-                     modificationtime,
-                     NVL(LEAD(CAST(modificationtime as date), 1)
-                              OVER (
-                                  PARTITION BY pandaid ORDER BY modificationtime ASC),
-                         trunc(to_date(:from_date, 'YYYY-MM-DD HH24:MI:SS'), 'HH24')) as lead_timestamp
-              FROM (SELECT *
-                    FROM jobs_statuses
-                    UNION ALL
-                    SELECT *
-                    FROM prev
-                   )
-              ),
-    jobs as (
-        SELECT pandaid,
-               queue,
-               gshare,
-               produsername,
-               transformation,
-               resource_type,
-                max(inputfilebytes) as inputfilebytes,
-                max(outputfilebytes) as outputfilebytes from (
-                                SELECT pandaid,
-                                      computingsite           as queue,
-                                      gshare,
-                                      produsername,
-                                      transformation,
-                                        resource_type,
-                                      NVL(inputfilebytes, 0)  as inputfilebytes,
-                                      NVL(outputfilebytes, 0) as outputfilebytes
-                               FROM ATLAS_PANDA.JOBSARCHIVED4
-                               WHERE pandaid IN (SELECT distinct pandaid FROM merge)
-                               UNION ALL
-                               SELECT pandaid,
-                                      computingsite           as queue,
-                                      gshare,
-                                      produsername,
-                                       transformation,
-                                        resource_type,
-                                      NVL(inputfilebytes, 0)  as inputfilebytes,
-                                      NVL(outputfilebytes, 0) as outputfilebytes
-                               FROM ATLAS_PANDAARCH.JOBSARCHIVED
-                               WHERE pandaid IN (SELECT distinct pandaid FROM merge)
-                               UNION ALL
-                               SELECT pandaid,
-                                      computingsite           as queue,
-                                      gshare,
-                                      produsername,
-                                        transformation,
-                                        resource_type,
-                                      NVL(inputfilebytes, 0)  as inputfilebytes,
-                                      NVL(outputfilebytes, 0) as outputfilebytes
-                               FROM ATLAS_PANDA.JOBSACTIVE4
-                               WHERE pandaid IN (SELECT distinct pandaid FROM merge)
-                               UNION ALL
-                               SELECT pandaid,
-                                      computingsite           as queue,
-                                      gshare,
-                                      produsername,
-                                       transformation,
-                                        resource_type,
-                                      NVL(inputfilebytes, 0)  as inputfilebytes,
-                                      NVL(outputfilebytes, 0) as outputfilebytes
-                               FROM ATLAS_PANDA.JOBSWAITING4
-                               WHERE pandaid IN (SELECT distinct pandaid FROM merge)
-                               UNION ALL
-                               SELECT pandaid,
-                                      computingsite           as queue,
-                                      gshare,
-                                      produsername,
-                                       transformation,
-                                        resource_type,
-                                      NVL(inputfilebytes, 0)  as inputfilebytes,
-                                      NVL(outputfilebytes, 0) as outputfilebytes
-                               FROM ATLAS_PANDA.JOBSDEFINED4
-                               WHERE pandaid IN (SELECT distinct pandaid FROM merge)
-                       )
-                group by pandaid, queue, gshare, produsername, transformation, resource_type
-    )
+with a as (SELECT start_time,
+                  end_time,
+                  pandaid,
+                  queue,
+                  status,
+                  CASE
+                      WHEN modificationtime < trunc(
+                                                      to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                                                      'HH24'
+                                                  ) - 1 / 24
+                          THEN trunc(to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                                     'HH24'
+                                   ) - 1 / 24
+                      ELSE modificationtime
+                      END as modificationtime,
+                  lead_time
+           FROM (SELECT trunc(
+                                to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                                'HH24'
+                            ) - 1 / 24 as start_time,
+                        trunc(
+                                to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                                'HH24'
+                            )          as end_time,
+                        pandaid,
+                        computingsite  as queue,
+                        jobstatus      as status,
+                        modificationtime,
+                        LEAD(CAST(modificationtime as date), 1) OVER (
+                            PARTITION BY pandaid
+                            ORDER BY modificationtime asc
+                            )          as lead_time
+                 FROM ATLAS_PANDA.JOBS_STATUSLOG
+                 WHERE modificationtime <
+                       trunc(
+                               to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                               'HH24'
+                           )
+                   and modificationtime >=
+                       trunc(
+                               to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                               'HH24'
+                           ) - 7
+                   and prodsourcelabel = 'user')
+           WHERE lead_time >= trunc(
+                                      to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                                      'HH24'
+                                  ) - 1 / 24
+              or lead_time is null),
+    b as (
+    SELECT start_time,
+                  end_time,
+                  pandaid,
+                  queue,
+                  status,
+                  MIN(modificationtime) as modificationtime,
+                  MAX(lead_time) as lead_time
+    FROM (
+    SELECT start_time,
+                  end_time,
+                  pandaid,
+                  queue,
+                  CASE
+                                 WHEN status in (
+                                                   'pending',
+                                                   'defined',
+                                                   'assigned',
+                                                   'activated',
+                                                   'throttled',
+                                                   'sent',
+                                                   'starting'
+                                     ) THEN 'waiting'
+                                 WHEN status in ('running', 'holding', 'merging', 'transferring') THEN 'executing'
+                                 END          as status,
+                  modificationtime,
+                  lead_time
+          FROM a
+          )
+    GROUP BY start_time,
+                  end_time,
+                  pandaid,
+                  queue,
+                  status
+          ),
+    c as (
         SELECT
-            trunc(to_date(:from_date, 'YYYY-MM-DD HH24:MI:SS'), 'HH24') as datetime,
-            pandaid,
-               queue,
-               gshare,
-               produsername,
+                  pandaid,
+                CASE
+                                 WHEN status in ('finished', 'failed', 'closed', 'cancelled') THEN status
+                                 END          as final_status
+          FROM a
+    ),
+    d as (SELECT b.start_time,
+                 b.end_time,
+                 b.pandaid,
+                 b.queue,
+                 b.status,
+                 c.final_status,
+                 round((b.lead_time - b.modificationtime) * 24 * 60 * 60) as duration
+          FROM b
+                   LEFT JOIN c
+                             ON (b.pandaid = c.pandaid)
+          WHERE c.final_status is not Null
+            and b.lead_time is not Null
+          ),
+    e as (
+    SELECT pandaid,
+        gshare,
+        produsername,
+        transformation,
+        resource_type,
+        cpuconsumptionunit,
+        proddblock,
+        processingtype,
+        inputfiletype,
+        inputfileproject,
+        max(inputfilebytes) as inputfilebytes,
+        max(outputfilebytes) as outputfilebytes
+    from (
+            SELECT pandaid,
+                gshare,
+                produsername,
                 transformation,
-                resource_type as job_resource_type,
-               common_status,
-               final_status,
-               MAX(inputfilebytes) as inputfilebytes,
-               MAX(outputfilebytes) as outputfilebytes,
-               min(modificationtime) as modificationtime,
-               max(lead_timestamp) as lead_timestamp,
-               round((max(lead_timestamp) - min(modificationtime))*60*60*24) as duration
-        FROM (SELECT m.pandaid,
-                     m.queue,
-                     j.gshare,
-                     j.produsername,
-                     j.transformation,
-                     j.resource_type,
-                     j.inputfilebytes as inputfilebytes,
-                     j.outputfilebytes,
-                     CASE
-                         WHEN m.status in
-                              ('pending', 'defined', 'assigned', 'activated', 'throttled', 'sent', 'starting')
-                             THEN 'queued'
-                         WHEN m.status in ('running', 'holding', 'merging', 'transferring')
-                             THEN 'executing'
-                         END               as common_status,
-                     CASE
-                         WHEN m.status in ('finished', 'failed', 'closed', 'cancelled')
-                             THEN m.status
-                         END as final_status,
-                     m.modificationtime,
-                     m.lead_timestamp
-              FROM merge m
-              JOIN jobs j ON (j.pandaid = m.pandaid)
-              )
-        GROUP BY
-            trunc(to_date(:from_date, 'YYYY-MM-DD HH24:MI:SS'), 'HH24'),
-            pandaid,
-               queue,
-               gshare,
-               produsername,
-               transformation,
                 resource_type,
-               common_status,
-               final_status
-        ORDER BY modificationtime
+                cpuconsumptionunit,
+                proddblock,
+                processingtype,
+                inputfiletype,
+                inputfileproject,
+                NVL(inputfilebytes, 0) as inputfilebytes,
+                NVL(outputfilebytes, 0) as outputfilebytes
+            FROM ATLAS_PANDA.JOBSARCHIVED4
+            WHERE pandaid IN (
+                    SELECT distinct pandaid
+                    FROM d
+                )
+                and modificationtime >= trunc(
+                               to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                               'HH24'
+                           ) - 7
+                and modificationtime < trunc(
+                                            to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                                            'HH24'
+                                        )
+                and prodsourcelabel = 'user'
+            UNION ALL
+            SELECT pandaid,
+                gshare,
+                produsername,
+                transformation,
+                resource_type,
+                cpuconsumptionunit,
+                proddblock,
+                processingtype,
+                inputfiletype,
+                inputfileproject,
+                NVL(inputfilebytes, 0) as inputfilebytes,
+                NVL(outputfilebytes, 0) as outputfilebytes
+            FROM ATLAS_PANDAARCH.JOBSARCHIVED
+            WHERE pandaid IN (
+                    SELECT distinct pandaid
+                    FROM d
+                )
+                and modificationtime >= trunc(
+                               to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                               'HH24'
+                           ) - 7
+                and modificationtime < trunc(
+                                            to_date(:end_date, 'YYYY-MM-DD HH24:MI:SS'),
+                                            'HH24'
+                                        )
+                and prodsourcelabel = 'user'
+            UNION ALL
+            SELECT pandaid,
+                gshare,
+                produsername,
+                transformation,
+                resource_type,
+                cpuconsumptionunit,
+                proddblock,
+                processingtype,
+                inputfiletype,
+                inputfileproject,
+                NVL(inputfilebytes, 0) as inputfilebytes,
+                NVL(outputfilebytes, 0) as outputfilebytes
+            FROM ATLAS_PANDA.JOBSACTIVE4
+            WHERE pandaid IN (
+                    SELECT distinct pandaid
+                    FROM d
+                )
+            UNION ALL
+            SELECT pandaid,
+                gshare,
+                produsername,
+                transformation,
+                resource_type,
+                cpuconsumptionunit,
+                proddblock,
+                processingtype,
+                inputfiletype,
+                inputfileproject,
+                NVL(inputfilebytes, 0) as inputfilebytes,
+                NVL(outputfilebytes, 0) as outputfilebytes
+            FROM ATLAS_PANDA.JOBSWAITING4
+            WHERE pandaid IN (
+                    SELECT distinct pandaid
+                    FROM d
+                )
+            UNION ALL
+            SELECT pandaid,
+                gshare,
+                produsername,
+                transformation,
+                resource_type,
+                cpuconsumptionunit,
+                proddblock,
+                processingtype,
+                inputfiletype,
+                inputfileproject,
+                NVL(inputfilebytes, 0) as inputfilebytes,
+                NVL(outputfilebytes, 0) as outputfilebytes
+            FROM ATLAS_PANDA.JOBSDEFINED4
+            WHERE pandaid IN (
+                    SELECT distinct pandaid
+                    FROM d
+                )
+        )
+    group by pandaid,
+        gshare,
+        produsername,
+        transformation,
+        resource_type,
+                        cpuconsumptionunit,
+                proddblock,
+                processingtype,
+                inputfiletype,
+                inputfileproject
+)
+SELECT d.start_time,
+       d.end_time,
+       d.pandaid,
+       d.queue,
+       d.status,
+       d.final_status,
+       d.duration,
+       e.gshare,
+       e.produsername,
+       e.transformation,
+       e.resource_type,
+       e.cpuconsumptionunit,
+       e.proddblock,
+       e.processingtype,
+       e.inputfiletype,
+       e.inputfileproject,
+       e.inputfilebytes,
+       e.outputfilebytes
+FROM d, e WHERE d.pandaid = e.pandaid
